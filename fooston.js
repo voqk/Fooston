cb86e80ba42b9603cb4ce35b2b81f5e5f3e1094f
Hunter Wright <HWright@slb.com>
2016-12-01 14:12:39 -0600

    updated ratings

--- fooston.js
+++ fooston.js
@@ -1,14 +1,16 @@
 function rankingToTableRow(ranking) {
+	const delta = parseInt(ranking['Change']);
+	const deltaClass = delta >= 0 ? 'label label-success' : 'label label-danger';
+
 	return `
 		<tr>
 			<td>${ranking.Rank}</td>
 			<td>${ranking.Standings}</td>
-			<td>${ranking.Rating}</td>
+			<td>${ranking.Rating} <span class="${deltaClass}">${delta}</span></td>
 			<td>${ranking['Games Back']}</td>
 			<td>${ranking['Prev Rank']}</td>
 			<td>${ranking['Prev Pnts']}</td>
 			<td>${ranking['Wks Off']}</td>
-			<td>${ranking['Change']}</td>
 		</tr>`;
 }
 
