5b41c5db545cfbfe70efc29094dcf5b84a4a103c
hunter <hunter@voqk.com>
2017-06-01 18:36:49 -0500

    update ratings

---
+++ ratings/ratings-2017-06-01.js
@@ -0,0 +1,354 @@
+const currentRankings = [
+ {
+   "Rank": 1,
+   "Standings": "WILL D",
+   "Rating": 2128,
+   "Games Back": "-",
+   "Prev Rank": 1,
+   "Prev Pnts": 2185,
+   "Wks Off": "-",
+   "Change": -57,
+   "+": {
+      "-": "7"
+   },
+   "Wins": 9,
+   "Losses": 2,
+   "Percent": "81.8%"
+},
+ {
+   "Rank": 2,
+   "Standings": "BILL V",
+   "Rating": 2065,
+   "Games Back": "0.5",
+   "Prev Rank": 4,
+   "Prev Pnts": 1945,
+   "Wks Off": "-",
+   "Change": 120,
+   "+": {
+      "-": "6"
+   },
+   "Wins": 8,
+   "Losses": 2,
+   "Percent": "80.0%"
+},
+ {
+   "Rank": 3,
+   "Standings": "MARK N",
+   "Rating": 2044,
+   "Games Back": "0.5",
+   "Prev Rank": 2,
+   "Prev Pnts": 2090,
+   "Wks Off": "-",
+   "Change": -46,
+   "+": {
+      "-": "3"
+   },
+   "Wins": 5,
+   "Losses": 2,
+   "Percent": "71.4%"
+},
+ {
+   "Rank": 4,
+   "Standings": "HUNTER W",
+   "Rating": 1914,
+   "Games Back": "1.5",
+   "Prev Rank": 7,
+   "Prev Pnts": 1797,
+   "Wks Off": "-",
+   "Change": 117,
+   "+": {
+      "-": "1"
+   },
+   "Wins": 3,
+   "Losses": 2,
+   "Percent": "60.0%"
+},
+ {
+   "Rank": 5,
+   "Standings": "TIM H",
+   "Rating": 1871,
+   "Games Back": "1.5",
+   "Prev Rank": 6,
+   "Prev Pnts": 1856,
+   "Wks Off": "-",
+   "Change": 15,
+   "+": {
+      "-": "2"
+   },
+   "Wins": 4,
+   "Losses": 2,
+   "Percent": "66.7%"
+},
+ {
+   "Rank": 6,
+   "Standings": "CHUCK E",
+   "Rating": 1858,
+   "Games Back": "1.5",
+   "Prev Rank": 5,
+   "Prev Pnts": 1877,
+   "Wks Off": "-",
+   "Change": -19,
+   "+": {
+      "-": "2"
+   },
+   "Wins": 6,
+   "Losses": 4,
+   "Percent": "60.0%"
+},
+ {
+   "Rank": 7,
+   "Standings": "BRETT W",
+   "Rating": 1765,
+   "Games Back": "2",
+   "Prev Rank": 9,
+   "Prev Pnts": 1707,
+   "Wks Off": "-",
+   "Change": 58,
+   "+": {
+      "-": "(1)"
+   },
+   "Wins": 1,
+   "Losses": 2,
+   "Percent": "33.3%"
+},
+ {
+   "Rank": 8,
+   "Standings": "CORY K",
+   "Rating": 1692,
+   "Games Back": "2.5",
+   "Prev Rank": 3,
+   "Prev Pnts": 1975,
+   "Wks Off": "-",
+   "Change": -283,
+   "+": {
+      "-": "(2)"
+   },
+   "Wins": 2,
+   "Losses": 4,
+   "Percent": "33.3%"
+},
+ {
+   "Rank": 9,
+   "Standings": "JAMES F",
+   "Rating": 1682,
+   "Games Back": "2.5",
+   "Prev Rank": 10,
+   "Prev Pnts": 1682,
+   "Wks Off": "-",
+   "Change": 0,
+   "+": {
+      "-": "(1)"
+   },
+   "Wins": 1,
+   "Losses": 2,
+   "Percent": "33.3%"
+},
+ {
+   "Rank": 10,
+   "Standings": "DAN J",
+   "Rating": 1669,
+   "Games Back": "2.5",
+   "Prev Rank": 8,
+   "Prev Pnts": 1739,
+   "Wks Off": "-",
+   "Change": -70,
+   "+": {
+      "-": "(3)"
+   },
+   "Wins": null,
+   "Losses": 3,
+   "Percent": "0.0%"
+},
+ {
+   "Rank": 11,
+   "Standings": "TIM D",
+   "Rating": 1489,
+   "Games Back": "3.5",
+   "Prev Rank": 16,
+   "Prev Pnts": 1406,
+   "Wks Off": "-",
+   "Change": 83,
+   "+": {
+      "-": "(2)"
+   },
+   "Wins": 2,
+   "Losses": 4,
+   "Percent": "33.3%"
+},
+ {
+   "Rank": 12,
+   "Standings": "DAN M",
+   "Rating": 1445,
+   "Games Back": "3.5",
+   "Prev Rank": 14,
+   "Prev Pnts": 1448,
+   "Wks Off": "-",
+   "Change": -3,
+   "+": {
+      "-": "(2)"
+   },
+   "Wins": null,
+   "Losses": 2,
+   "Percent": "0.0%"
+},
+ {
+   "Rank": 13,
+   "Standings": "KEITH R",
+   "Rating": 1441,
+   "Games Back": "3.5",
+   "Prev Rank": 11,
+   "Prev Pnts": 1507,
+   "Wks Off": "-",
+   "Change": -66,
+   "+": {
+      "-": "(2)"
+   },
+   "Wins": null,
+   "Losses": 2,
+   "Percent": "0.0%"
+},
+ {
+   "Rank": 14,
+   "Standings": "KIM B",
+   "Rating": 1382,
+   "Games Back": "4",
+   "Prev Rank": 15,
+   "Prev Pnts": 1407,
+   "Wks Off": "-",
+   "Change": -25,
+   "+": {
+      "-": "(3)"
+   },
+   "Wins": 1,
+   "Losses": 4,
+   "Percent": "20.0%"
+},
+ {
+   "Rank": 15,
+   "Standings": "DAN O",
+   "Rating": 1348,
+   "Games Back": "4",
+   "Prev Rank": 19,
+   "Prev Pnts": 1269,
+   "Wks Off": "-",
+   "Change": 79,
+   "+": {
+      "-": "(2)"
+   },
+   "Wins": 2,
+   "Losses": 4,
+   "Percent": "33.3%"
+},
+ {
+   "Rank": 16,
+   "Standings": "BEN F",
+   "Rating": 1337,
+   "Games Back": "4",
+   "Prev Rank": 12,
+   "Prev Pnts": 1485,
+   "Wks Off": "1",
+   "Change": -148,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+},
+ {
+   "Rank": 17,
+   "Standings": "ELLIE M",
+   "Rating": 1170,
+   "Games Back": "5",
+   "Prev Rank": 21,
+   "Prev Pnts": 1175,
+   "Wks Off": "-",
+   "Change": -5,
+   "+": {
+      "-": "(1)"
+   },
+   "Wins": null,
+   "Losses": 1,
+   "Percent": "0.0%"
+},
+ {
+   "Rank": 18,
+   "Standings": "ANDREW T",
+   "Rating": 1154,
+   "Games Back": "5",
+   "Prev Rank": 13,
+   "Prev Pnts": 1484,
+   "Wks Off": "2",
+   "Change": -330,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+},
+ {
+   "Rank": 19,
+   "Standings": "RAYDON C",
+   "Rating": 1002,
+   "Games Back": "5.5",
+   "Prev Rank": 18,
+   "Prev Pnts": 1289,
+   "Wks Off": "2",
+   "Change": -287,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+},
+ {
+   "Rank": 20,
+   "Standings": "MOUNIR T",
+   "Rating": 850,
+   "Games Back": "6.5",
+   "Prev Rank": 20,
+   "Prev Pnts": 1189,
+   "Wks Off": "3",
+   "Change": -339,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+},
+ {
+   "Rank": 21,
+   "Standings": "JUAN P",
+   "Rating": 584,
+   "Games Back": "7.5",
+   "Prev Rank": 28,
+   "Prev Pnts": 818,
+   "Wks Off": "3",
+   "Change": -234,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+},
+ {
+   "Rank": 22,
+   "Standings": "MAX B",
+   "Rating": 575,
+   "Games Back": "8",
+   "Prev Rank": 30,
+   "Prev Pnts": 805,
+   "Wks Off": "3",
+   "Change": -230,
+   "+": {
+      "-": " "
+   },
+   "Wins": null,
+   "Losses": null,
+   "Percent": ""
+}
+]
